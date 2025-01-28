4b9a05ee8af79bf125201372a4534e4c69261dce
salman-itShaala <salman.shaikh@itshaala.com>
2025-01-28 16:38:34 +0530

    added input validation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const todoInput = document.getElementById("todo-input");
 const todosWrapper = document.querySelector(".todos-wrapper");
 
-const todos = []; // [{title: "", isCompleted : false, id : }]
+const todosArray = []; // [{title: "", isCompleted : false, id : }]
 
 function handleAddTodo() {
-  // add todo
-  console.log(todoInput.value);
-  //   {title: "", isCompleted : false, id : }
+  if (!todoInput.value) {
+    alert("Add Valid input...");
+    return;
+  }
 
   const todoObj = {
     title: todoInput.value,
@@ -14,23 +15,25 @@ function handleAddTodo() {
     id: Date.now(),
   };
 
-  todos.push(todoObj);
+  todosArray.push(todoObj);
 
   // front - end --> todos
+  updateUi();
 
-  updateUi(todos);
+  todoInput.value = "";
 }
 
-function updateUi(todos) {
+function updateUi() {
   todosWrapper.innerHTML = "";
 
-  for (let i = 0; i < todos.length; i++) {
+  for (let i = 0; i < todosArray.length; i++) {
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
 
     const todoPara = document.createElement("p");
-    todoPara.innerText = todos[i].title;
+    todoPara.innerText = todosArray[i].title;
 
+    // appending paragraph to todo div
     todoDiv.appendChild(todoPara);
 
     todosWrapper.append(todoDiv);
